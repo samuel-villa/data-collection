aa68acab59d15fe7ea447232dba40b92616868d8
sam <ciullasamuel@gmail.com>
2022-02-03 23:39:38 +0100

    [IMP] code structure + use of a better scraping library

--- udemy_courses/udemy_courses.py
+++ udemy_courses/udemy_courses.py
@@ -1,11 +1,27 @@
 """
 Collecting the full list of Udemy based courses
 """
-from urllib.request import urlopen
+import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.udemy.com/sitemap/"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+target_url = "https://www.udemy.com/sitemap/"
+headers = {}
+params = ()
 
-print(html)
+
+def html_parser(url, h=None, p=None):
+    """
+    creating the connection with the target website
+    :param url: target url
+    :param h: headers
+    :param p: parameters
+    :return: BeautifulSoup object
+    """
+    response = requests.get(url, headers=h, params=p)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+
+print(type(html_parser(target_url)))
+
