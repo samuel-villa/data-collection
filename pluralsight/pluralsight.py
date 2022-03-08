32f14c15b2def2ea38e6f742a47428925c1731a3
sam <ciullasamuel@gmail.com>
2022-03-08 22:45:05 +0100

    [ADD] error urls process automation

--- pluralsight/pluralsight.py
+++ pluralsight/pluralsight.py
@@ -6,6 +6,8 @@ Collecting the full list of PluralSight based courses
     * store data into dictionary
     * push dictionary into a json file
     * create info log file
+    * problematic urls are stored in order to be processed later on
+    * once the main scraping is finished, the problematic urls are processed again for a max number of attempts
 """
 import collector_tools
 from datetime import datetime
@@ -14,6 +16,7 @@ _CATEGORY = 'education'
 _NAME = 'pluralsight'
 SITEMAP_URL = "https://www.pluralsight.com/sitemap.xml"
 COURSES_URL = "https://www.pluralsight.com/courses/"
+ERROR_URLS_MAX_ATTEMPTS = 3
 
 data_path = collector_tools.create_storage_dir(_CATEGORY, _NAME)
 json_filename = data_path + _NAME + '.json'
@@ -37,15 +40,44 @@ def _get_courses_links():
     return list(dict.fromkeys(courses_links))  # remove duplicates if any
 
 
-testing_links = [
-    "https://www.pluralsight.com/courses/insights-observations-data-executive-briefing",
-    "https://www.pluralsight.com/courses/your-first-day-rotoscoping-nuke-1217",
-    # "https://www.pluralsight.com/courses/inverted-triangle-css-intro",
-    # "https://www.pluralsight.com/courses/inverted-triangle-ERROR_TEST",
-    # "https://www.pluralsight.com/courses/java-unit-testing-junit",  # redirection
-    # "https://www.pluralsight.com/courses/querying-converting-ERROR_TEST",
-    # "https://www.pluralsight.com/courses/building-features-image-data",
-]
+def _scrape_html(url, soup, dict_keys):
+    """
+    Scrape PluralSight url in order to get specific values
+    :param url: [str] PluralSight url to scrape
+    :param soup: BeautifulSoup object
+    :param dict_keys: [dict] reset dictionary containing all specific keys we want to scrape
+    :return:
+    """
+    dict_keys["prod_id"] = soup.find("meta", {"name": "prodId"}).get('content') if soup.find("meta", {
+        "name": "prodId"}) else None
+    dict_keys["url"] = url
+    dict_keys["title"] = soup.find(id='course-page-hero').find('h1').text if soup.find(id='course-page-hero') else None
+    dict_keys["thumbnail"] = soup.find("meta", {"name": "thumbnail"}).get('content') if soup.find("meta", {
+        "name": "thumbnail"}) else None
+    dict_keys["description"] = soup.find("meta", {"name": "description"}).get('content') if soup.find("meta", {
+        "name": "description"}) else None
+    dict_keys["authors"] = soup.find("meta", {"name": "authors"}).get('content') if soup.find("meta", {
+        "name": "authors"}) else None
+    dict_keys["authors_about"] = soup.find("div", {"class": "author-item"}).find("p").text if soup.find("div", {
+        "class": "author-item"}) else None
+    dict_keys["authors_url"] = soup.find("div", {"class": "author-item"}).find('a').get('href') if soup.find("div", {
+        "class": "author-item"}) else None
+    dict_keys["roles"] = soup.find("meta", {"name": "roles"}).get('content') if soup.find("meta",
+                                                                                     {"name": "roles"}) else None
+    dict_keys["skill_levels"] = soup.find("meta", {"name": "skill-levels"}).get('content') if soup.find("meta", {
+        "name": "skill-levels"}) else None
+    dict_keys["publish_date"] = soup.find("meta", {"name": "publish-date"}).get('content') if soup.find("meta", {
+        "name": "publish-date"}) else None
+    dict_keys["rating"] = soup.find("meta", {"name": "rating"}).get('content') if soup.find("meta",
+                                                                                       {"name": "rating"}) else None
+    dict_keys["rating_count"] = soup.find("meta", {"name": "rating-count"}).get('content') if soup.find("meta", {
+        "name": "rating-count"}) else None
+    dict_keys["duration"] = soup.find("meta", {"name": "duration"}).get('content') if soup.find("meta", {
+        "name": "duration"}) else None
+    dict_keys["retired"] = soup.find("meta", {"name": "retired"}).get('content') if soup.find("meta", {
+        "name": "retired"}) else None
+    dict_keys["updated_date"] = soup.find("meta", {"name": "updated-date"}).get('content') if soup.find("meta", {
+        "name": "updated-date"}) else None
 
 
 def main():
@@ -68,8 +100,16 @@ def main():
         'updated_date': '',
     }
 
-    global_work_start_time = datetime.now()
+    testing_links = [
+        "https://www.pluralsight.com/courses/insights-observations-data-executive-briefing",
+        "https://www.pluralsight.com/courses/your-first-day-rotoscoping-nuke-1217",
+        "https://www.pluralsight.com/courses/inverted-triangle-css-intro",
+        "https://www.pluralsight.com/courses/inverted-triangle-ERROR_TEST",
+        # "https://www.pluralsight.com/courses/java-unit-testing-junit",  # redirection
+        "https://www.pluralsight.com/courses/querying-converting-ERROR_TEST",
+    ]
 
+    global_work_start_time = datetime.now()
     courses_lks = _get_courses_links()  # 13147
     courses_counter = 1
     error_links = []
@@ -82,22 +122,7 @@ def main():
         try:
             soup = collector_tools.get_soup(lk)
             keys = keys.fromkeys(keys)  # reset dict values
-            keys["prod_id"] = soup.find("meta", {"name": "prodId"}).get('content') if soup.find("meta", {"name": "prodId"}) else None
-            keys["url"] = lk
-            keys["title"] = soup.find(id='course-page-hero').find('h1').text if soup.find(id='course-page-hero') else None
-            keys["thumbnail"] = soup.find("meta", {"name": "thumbnail"}).get('content') if soup.find("meta", {"name": "thumbnail"}) else None
-            keys["description"] = soup.find("meta", {"name": "description"}).get('content') if soup.find("meta", {"name": "description"}) else None
-            keys["authors"] = soup.find("meta", {"name": "authors"}).get('content') if soup.find("meta", {"name": "authors"}) else None
-            keys["authors_about"] = soup.find("div", {"class": "author-item"}).find("p").text if soup.find("div", {"class": "author-item"}) else None
-            keys["authors_url"] = soup.find("div", {"class": "author-item"}).find('a').get('href') if soup.find("div", {"class": "author-item"}) else None
-            keys["roles"] = soup.find("meta", {"name": "roles"}).get('content') if soup.find("meta", {"name": "roles"}) else None
-            keys["skill_levels"] = soup.find("meta", {"name": "skill-levels"}).get('content') if soup.find("meta", {"name": "skill-levels"}) else None
-            keys["publish_date"] = soup.find("meta", {"name": "publish-date"}).get('content') if soup.find("meta", {"name": "publish-date"}) else None
-            keys["rating"] = soup.find("meta", {"name": "rating"}).get('content') if soup.find("meta", {"name": "rating"}) else None
-            keys["rating_count"] = soup.find("meta", {"name": "rating-count"}).get('content') if soup.find("meta", {"name": "rating-count"}) else None
-            keys["duration"] = soup.find("meta", {"name": "duration"}).get('content') if soup.find("meta", {"name": "duration"}) else None
-            keys["retired"] = soup.find("meta", {"name": "retired"}).get('content') if soup.find("meta", {"name": "retired"}) else None
-            keys["updated_date"] = soup.find("meta", {"name": "updated-date"}).get('content') if soup.find("meta", {"name": "updated-date"}) else None
+            _scrape_html(lk, soup, keys)
             collector_tools.push_data2json(json_filename, keys, _NAME)
             log = "DONE\n"
             collector_tools.write_log(log_filename, log)
@@ -137,6 +162,26 @@ def main():
     else:
         log_err_msg = f"No errors"
         collector_tools.write_log(log_filename, log_err_msg)
+
+    attempt = 0
+    log_ricover = "\n====================== RICOVER URLS ======================\n\n"
+    collector_tools.write_log(log_filename, log_ricover)
+    while attempt < ERROR_URLS_MAX_ATTEMPTS:
+        for url in error_links:
+            log = f"attempt {attempt + 1} - working on: {url} ... "
+            collector_tools.write_log(log_filename, log)
+            try:
+                soup = collector_tools.get_soup(url)
+                keys = keys.fromkeys(keys)
+                _scrape_html(url, soup, keys)
+                collector_tools.push_data2json(json_filename, keys, _NAME)
+                log = f"{url} RICOVERED\n"
+                collector_tools.write_log(log_filename, log)
+                error_links.remove(url)
+            except TypeError:
+                log = "ERROR <==================\n"
+                collector_tools.write_log(log_filename, log)
+        attempt += 1
 
 
 if __name__ == '__main__':
