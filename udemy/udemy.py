55312ce58b545a13325463bbceacee03e78a531c
sam <ciullasamuel@gmail.com>
2022-04-18 23:39:44 +0200

    [IMP] udemy storage folders

--- udemy/udemy.py
+++ udemy/udemy.py
@@ -8,45 +8,27 @@ Collecting the full list of Udemy based courses
         - push all collected courses data into a json file,
     * push all collected categories into a global json file
     * create info log file
-
-TODO make data be stored in the common 'data_storage' folder
 """
-
 import os
 import requests
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime
-import collector_tools
+import collector_tools as ct
 
 _CATEGORY = 'education'
 _NAME = 'udemy'
+_CATEGORIES_PATH = 'data_divided_in_categories'
+
 SITEMAP_URL = "https://www.udemy.com/sitemap/"
 BASE_URL = "https://www.udemy.com"
-DATA_DIR = "./data/"
-UDEMY_COURSES_DIR_NAME = "udemy_courses_data/"
-MERGED_COURSES_FILENAME = "udemy_courses_full_list"
-GLOBAL_ROOT_KEY = "courses_by_topic"
-
-# build directories tree
-date_now = datetime.now()  # 2022-02-22 22:22:02.228866
-timestamp = str(date_now).split(".")[0].replace("-", "").replace(" ", "_").replace(":", "")  # 20220222_222202
-root_path = DATA_DIR + timestamp + "/"  # data/20220222_222202/
-udemy_courses_path = root_path + UDEMY_COURSES_DIR_NAME
-log_filename = root_path + timestamp + ".log"
-merged_courses_file_path = root_path + MERGED_COURSES_FILENAME + ".json"
-zip_file_name = root_path + timestamp
-
-# create directories if they don't exist
-os.makedirs(DATA_DIR, exist_ok=True)
-os.makedirs(udemy_courses_path, exist_ok=True)
 
 
 def get_topic_id(topic_link):
     """
-    parse the topic url in order to fetch the category id
+    Parse the topic url in order to fetch its id
     :param topic_link: category url
-    :return: category id
+    :return: topic id
     """
     url = BASE_URL + topic_link
     response = requests.get(url)
@@ -61,7 +43,7 @@ def get_topic_id(topic_link):
 
 def collect_topic_courses(cat_id, json_fn, category_name):
     """
-    get all courses items of one given category (topic) and push collected data to json file
+    Get all courses items of one given category (topic) and push collected data to json file
     :param category_name: category name
     :param json_fn: json filename
     :param cat_id: courses category id
@@ -84,7 +66,7 @@ def collect_topic_courses(cat_id, json_fn, category_name):
             for i in json_data['unit']['items']:
                 if i["id"] not in courses_ids:  # check for duplicate courses
                     courses_ids.append(i["id"])
-                    push_data2json(json_fn, i, category_name)
+                    ct.push_data2json(json_fn, i, category_name)
                     courses_cnt += 1
         else:
             print("NO DATA collected for topic ID " + str(cat_id) + " at page: " + str(current_page) +
@@ -99,58 +81,16 @@ def collect_topic_courses(cat_id, json_fn, category_name):
     return courses_cnt
 
 
-def init_json_file(json_filename, root_key):
-    """
-    create new json file and initialize it with generic empty dictionary
-    :param json_filename: JSON file name
-    :param root_key: json root key name
-    """
-    root = {root_key: []}
-    json_object = json.dumps(root, indent=4)
-    with open(json_filename, "w") as file:
-        file.write(json_object)
-
-
-def push_data2json(filename, data, root_key):
-    """
-    append data to existing json file
-    :param filename: json file name
-    :param data: data to be appended
-    :param root_key: category name
-    """
-    with open(filename, 'r+') as f:
-        file_data = json.load(f)  # load existing data
-        file_data[root_key].append(data)  # join new_data with file_data inside udemy
-        f.seek(0)  # sets f's current position at offset
-        json.dump(file_data, f, indent=4)  # convert back to json
-
-
-def merge_categories2json(new_file, root, cat_dir):
-    """
-    Merge all categories json files into one json file
-    :param new_file: global filename
-    :param root: root key name (bypassed, useless)
-    :param cat_dir: categories json files directory
-    """
-    init_json_file(new_file, root)
-    data = {root: []}
-    for jsonfile in os.listdir(cat_dir):
-        json_filepath = cat_dir + jsonfile
-        with open(json_filepath, 'r+') as infile:
-            data[root].append(json.load(infile))
-        with open(new_file, 'r+') as outfile:
-            json.dump(data, outfile, indent=4)
-
-
 def main():
     """
     loop through all categories and collect all courses data
     creates a log file
     """
     global_work_start_time = datetime.now()
+    files = ct.init_data_storage_dir_for_categories(_CATEGORY, _NAME, _CATEGORIES_PATH)
 
-    soup = collector_tools.get_soup(SITEMAP_URL)
-    topic_endpoints = collector_tools.get_endpoints_containing("/topic/", soup)  # len=341
+    soup = ct.get_soup(SITEMAP_URL)
+    topic_endpoints = ct.get_endpoints_containing("/topic/", soup)  # len=341
     unique_topic_endpoints = list(dict.fromkeys(topic_endpoints))  # removing duplicates (len=276)
 
     total_categories = 0
@@ -162,10 +102,9 @@ def main():
 
         topic_id = get_topic_id(link)  # 8322
         topic_name = str(link).split("/")[2]  # "web-development"
-        data_filename = udemy_courses_path + str(topic_id) + "_" + str(topic_name) + ".json"
-        # data/20220222_222202/udemy_courses_data/8322_web-development.json
+        data_filename = files['categories_path'] + str(topic_id) + "_" + str(topic_name) + ".json"
 
-        init_json_file(data_filename, topic_name)
+        ct.init_json_file(data_filename, topic_name)
         print(f"CATEGORY: {topic_name}")
         print(f"file '{data_filename}' initialized")
         work_start_time = datetime.now()
@@ -175,7 +114,7 @@ def main():
         print(f"{courses_count} courses collected in {work_duration}")
         print("============================================================\n")
 
-        with open(log_filename, 'a') as f:
+        with open(files['log_filename'], 'a') as f:
             f.write(f"CATEGORY: {topic_name}\n")
             f.write(f"{courses_count} courses collected in {work_duration}\n\n")
 
@@ -187,11 +126,11 @@ def main():
 
     # merging categories into one global file + calculate work time
     merging_start_time = datetime.now()
-    merge_categories2json(merged_courses_file_path, GLOBAL_ROOT_KEY, udemy_courses_path)
+    ct.make_single_json_from_categories(files['json_filename'], _NAME, files['categories_path'])
     merging_end_time = datetime.now()
     merging_duration = merging_end_time - merging_start_time
 
-    with open(log_filename, 'a') as f:
+    with open(files['log_filename'], 'a') as f:
         f.write("====================== TOTAL ======================\n\n")
         f.write(f"Date           : {global_work_start_time}\n")
         f.write(f"Nb. categories : {total_categories}\n")
